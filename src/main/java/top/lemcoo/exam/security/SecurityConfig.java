fcefc02e217c65475693e28a6ddb6f4d668d4beb
zhaowx <zhaoweixu@zg-sst.com>
2021-07-14 09:04:30 +0800

    跨域

--- src/main/java/top/lemcoo/exam/security/SecurityConfig.java
+++ src/main/java/top/lemcoo/exam/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import sun.security.util.SecurityConstants;
 import top.lemcoo.exam.service.MyUserDetailsService;
+
+import java.util.Arrays;
+
+import static java.util.Collections.singletonList;
 
 /**
  * 【security配置】
@@ -38,6 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${jwt.route.authPath}")
     private String authPath;
+    @Value("${jwt.header}")
+    private String jwtHeader;
 
     /**
      * 密码加密策略
@@ -81,6 +92,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 处理异常情况：认证失败和权限不足
         http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
-        // http.cors();
+        http.cors(Customizer.withDefaults());
+    }
+
+    /**
+     * Cors配置优化
+     **/
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        org.springframework.web.cors.CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(singletonList("*"));
+        // configuration.setAllowedOriginPatterns(singletonList("*"));
+        configuration.setAllowedHeaders(singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+        configuration.setExposedHeaders(singletonList(jwtHeader));
+        configuration.setAllowCredentials(false);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
